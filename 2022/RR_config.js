fed7a69a12dccd048fe837dd87d5f8147cd3a625
markzakharyan <56100600+markzakharyan@users.noreply.github.com>
2022-09-07 22:15:53 -0700

    fixed thingy

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"",
+        "defaultValue":"2022OCFAIR",
         "required":"true",
         "disabled":"true"
       },
